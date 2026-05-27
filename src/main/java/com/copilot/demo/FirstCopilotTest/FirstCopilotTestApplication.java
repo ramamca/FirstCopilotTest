7b654c8c6d0ea9de85789c10b5ae8f2a43c7ab47
TWINKLE <rama.mca27@gmail.com>
2026-05-27 22:24:07 +0530

    Revert "third commit"
    This reverts commit ff8efab0ccfc922704c0e3f57a9ec50324865d39.

--- src/main/java/com/copilot/demo/FirstCopilotTest/FirstCopilotTestApplication.java
+++ src/main/java/com/copilot/demo/FirstCopilotTest/FirstCopilotTestApplication.java
@@ -2,9 +2,8 @@ package com.copilot.demo.FirstCopilotTest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class FirstCopilotTestApplication {
 
 	public static void main(String[] args) {
