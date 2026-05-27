b1fcf8fbaa75901acf1fb08912256a12652bf53b
TWINKLE <rama.mca27@gmail.com>
2026-05-27 22:01:28 +0530

    First commit

---
+++ src/main/java/com/copilot/demo/FirstCopilotTest/FirstCopilotTestApplication.java
@@ -0,0 +1,13 @@
+package com.copilot.demo.FirstCopilotTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstCopilotTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstCopilotTestApplication.class, args);
+	}
+
+}
